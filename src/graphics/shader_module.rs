ce1e3b55050723791fb2bab97f385ec31ee8377a
Hyjale <saishidual@gmail.com>
2022-12-27 14:39:47 -0500

    Shader module

---
+++ src/graphics/shader_module.rs
@@ -0,0 +1,22 @@
+use std::{io::Cursor};
+
+use ash::{
+    util::read_spv,
+    vk::{self}
+};
+
+pub struct ShaderModule {}
+
+impl ShaderModule {
+    pub fn new(device: &ash::Device, path: &[u8]) -> ash::vk::ShaderModule {
+        unsafe {
+            let code = read_spv(&mut Cursor::new(path)).unwrap();
+
+            let shader_module = device
+                .create_shader_module(&vk::ShaderModuleCreateInfo::builder().code(&code), None)
+                .unwrap();
+
+            shader_module
+        }
+    }
+}
